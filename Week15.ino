acfaf3adf1b6575c4ecabca7e81c9ba002b1a8a9
YueChuCheng <39110560+YueChuCheng@users.noreply.github.com>
2018-06-06 11:06:57 +0800

    Add files via upload

---
+++ Week15.ino
@@ -0,0 +1,23 @@
+const int analogInPin=A0;
+ const int analogOutPin=9;
+ 
+  int outputValue=0;
+
+
+void setup() {
+  Serial.begin(9600);// put your setup code here, to run once:
+ 
+}
+
+void loop() {
+   int sensorRead=analogRead(A0);
+
+ Serial.println(sensorRead);
+  if(sensorRead<700){sensorRead=700;}
+  else if(sensorRead>900){sensorRead=900;}
+  outputValue=map(sensorRead,700,900,0,255);
+  analogWrite(analogOutPin,outputValue);
+ // put your main code here, to run repeatedly:
+Serial.println(outputValue);
+delay(2);
+}
